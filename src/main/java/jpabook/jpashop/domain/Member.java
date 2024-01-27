2fb245c7137d398782848065b4b6d4e0c3fab349
s0nnyday <lijnar100@naver.com>
2024-01-27 23:02:28 +0900

    JPA와 DB설정, 동작확인 + 외부라이브러리 P6Spy 쿼리 파라미터 로그

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -12,15 +12,19 @@ import java.util.List;
 @Setter
 public class Member {
 
+//    @Id @GeneratedValue
+//    @Column(name = "member_id")
+//    private Long id;
+//
+//    private String name;
+//
+//    @Embedded
+//    private Address address;
+//
+//    @OneToMany(mappedBy = "member")
+//    private List<Order> orders = new ArrayList<>();
+
     @Id @GeneratedValue
-    @Column(name = "member_id")
     private Long id;
-
-    private String name;
-
-    @Embedded
-    private Address address;
-
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
+    private String username;
 }
