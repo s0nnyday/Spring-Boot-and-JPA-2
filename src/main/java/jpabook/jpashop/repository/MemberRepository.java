6b1725e9e8f41c2d7d1b49f330e8f98b6b8b4e45
s0nnyday <lijnar100@naver.com>
2024-01-31 22:03:43 +0900

    회원 서비스 개발
    @Transactional 과 생성자 의존성 주입(final키워드+@RequiredArgsConstroctor)

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -4,14 +4,17 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
+    @Autowired
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
