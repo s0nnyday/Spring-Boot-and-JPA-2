cabb2e58643c3bac313cff801fc0b392ade4ac65
s0nnyday <lijnar100@naver.com>
2024-02-05 23:39:18 +0900

    chore: OrderRepository 정리

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -6,12 +6,16 @@ import jakarta.persistence.EntityManager;
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
+
     private final EntityManager em;
+
     public void save(Order order) {
         em.persist(order);
     }
+
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
+
     //    public List<Order> findAll(OrderSearch orderSearch) { ... }
 }
