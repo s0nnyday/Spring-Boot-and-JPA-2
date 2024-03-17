9bf9efc7feaaebf8c026c9b48f22ddf3e33730a6
s0nnyday <lijnar100@naver.com>
2024-03-17 15:36:04 +0900

    JPA2 Spring Data Jpa 소개 오류 수정
    findById() -> findById().get()
    기본 findById()는 Optional 반환해서 변경

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -34,7 +34,7 @@ public class MemberServiceTest {
         Long savedId = memberService.join(member);
 
         //then
-        assertEquals(member, memberRepository.findById(savedId));
+        assertEquals(member, memberRepository.findById(savedId).get());
 
     }
 
